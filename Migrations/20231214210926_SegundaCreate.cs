def0fb4569f0d59ae2e53cc6bfae3b1210092513
Daniel <CacheStorage.2023111TADS0039@AudioWorkletNode.ifpi.edu.br>
2023-12-14 21:51:52 -0300

    Dotnet commit

---
+++ Migrations/20231214210926_SegundaCreate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DanielFinalDotnet.Migrations
+{
+    public partial class SegundaCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
